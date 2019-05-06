7a9547f55de9d6396ca416edad281e1d3acca3ee
Arturo Andrade <arturoandradm@gmail.com>
2019-05-06 11:48:06 -0500

    SE CORRIGIÓ UN ERROR DE RESPONSIBIDAD EN EL PIE DE PAGINA

--- partials/footer.php
+++ partials/footer.php
@@ -1,72 +1,90 @@
 <div class="block block-pd-sm bg-primary">
     <div class="container">
         <div class="row">
-        <div class="col-md-2"><h4 class="text-white"> Somos Distribuidores </h4></div>
-        <div class="col-md-10">
-            <div class="cont1">
-                <div>
-                    <a href="https://bootstrapmade.com" title="Client 6">
-                        <img src="http://placehold.it/120x60" alt="Client 6 logo" class="img-responsive">
-                    </a>
-                </div>
-                <div>
-                    <a href="https://bootstrapmade.com" title="Client 6">
-                        <img src="http://placehold.it/120x60" alt="Client 6 logo" class="img-responsive">
-                    </a>
-                </div>
-                <div>
-                    <a href="https://bootstrapmade.com" title="Client 6">
-                        <img src="http://placehold.it/120x60" alt="Client 6 logo" class="img-responsive">
-                    </a>
-                </div>
-                <div>
-                    <a href="https://bootstrapmade.com" title="Client 6">
-                        <img src="http://placehold.it/120x60" alt="Client 6 logo" class="img-responsive">
-                    </a>
-                </div>
-                <div>
-                    <a href="https://bootstrapmade.com" title="Client 6">
-                        <img src="http://placehold.it/120x60" alt="Client 6 logo" class="img-responsive">
-                    </a>
-                </div>
-                <div>
-                    <a href="https://bootstrapmade.com" title="Client 6">
-                        <img src="http://placehold.it/120x60" alt="Client 6 logo" class="img-responsive">
-                    </a>
+            <div class="col-md-2">
+                <h4 class="text-white"> Somos Distribuidores </h4>
+            </div>
+            <div class="col md-10">
+                <div class="cont1">
+                    <div>
+                        <a href="https://bootstrapmade.com" title="Client 6">
+                            <img src="http://placehold.it/120x60" alt="Client 6 logo" class="img-responsive">
+                        </a>
+                    </div>
+                    <div>
+                        <a href="https://bootstrapmade.com" title="Client 6">
+                            <img src="http://placehold.it/120x60" alt="Client 6 logo" class="img-responsive">
+                        </a>
+                    </div>
+                    <div>
+                        <a href="https://bootstrapmade.com" title="Client 6">
+                            <img src="http://placehold.it/120x60" alt="Client 6 logo" class="img-responsive">
+                        </a>
+                    </div>
+                    <div>
+                        <a href="https://bootstrapmade.com" title="Client 6">
+                            <img src="http://placehold.it/120x60" alt="Client 6 logo" class="img-responsive">
+                        </a>
+                    </div>
+                    <div>
+                        <a href="https://bootstrapmade.com" title="Client 6">
+                            <img src="http://placehold.it/120x60" alt="Client 6 logo" class="img-responsive">
+                        </a>
+                    </div>
+                    <div>
+                        <a href="https://bootstrapmade.com" title="Client 6">
+                            <img src="http://placehold.it/120x60" alt="Client 6 logo" class="img-responsive">
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
         </div>
     </div>
 </div>
 
+<!-- <div class="dis">
+    <div>
+        <h5>Somos Distribuidores</h5>
+    </div>
     
+    <div> <img src="http://placehold.it/180x80"> </div>
+    <div> <img src="http://placehold.it/180x80"></div>
+    <div> <img src="http://placehold.it/180x80"></div>
+    <div> <img src="http://placehold.it/180x80"></div>
+    <div><img src="http://placehold.it/180x80"></div>
+    <div><img src="http://placehold.it/180x80"></div>
+</div> -->
+
+
 <footer class=" bg-dark">
     <div class="container">
-        <div class="row"><div class="col-sm-3">
-            <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
-        </div>
-        <div class="col-sm text-white">
-            <p>¿Que hacemos?</p>
-            <ul>
-                <li id="item">
-                    <a href="#" class="text-white text-lighten-4">Paginas web</a>
-                </li>
-                <li id="item">
-                    <a href="#" class="text-white text-lighten-4">Soporte Tecnico</a>
-                </li>
-                <li id="item">
-                    <a href="#" class="text-white text-lighten-4" >Puntos de Venta</a>
-                </li>
-                <li id="item">
-                    <a href="" class="text-white text-lighten-4">Checadores</a>
-                </li>
-                <li id="item">
-                    <a href="" class="text-white text-lighten-4">y mas</a>
-                </li>
-            </ul>
-        </div>
-        <div class="col-sm text-light d-none d-lg-block"><p>Redes Sociales</p></div>
+        <div class="row">
+            <div class="col-sm-3">
+                <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+            </div>
+            <div class="col-sm text-white">
+                <p>¿Que hacemos?</p>
+                <ul>
+                    <li id="item">
+                        <a href="#" class="text-white text-lighten-4">Paginas web</a>
+                    </li>
+                    <li id="item">
+                        <a href="#" class="text-white text-lighten-4">Soporte Tecnico</a>
+                    </li>
+                    <li id="item">
+                        <a href="#" class="text-white text-lighten-4">Puntos de Venta</a>
+                    </li>
+                    <li id="item">
+                        <a href="" class="text-white text-lighten-4">Checadores</a>
+                    </li>
+                    <li id="item">
+                        <a href="" class="text-white text-lighten-4">y mas</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-sm text-light d-none d-lg-block">
+                <p>Redes Sociales</p>
+            </div>
             <div class="col-sm text-light">
                 <p>Contacto
                     <br>
@@ -78,18 +96,48 @@
     </div>
 </footer>
 <style>
-.cont1{
-  max-height: 180px;
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
-  grid-template-rows: 1fr;
+/* .dis {
+    align-items: center;
+    background-color: #2b72e5;
+    color: white;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+    /* este atributo indica el numero de columnas que tendra el grid */
+/* grid-gap: 10px;
+}
+
+
+@media only screen and (max-width: 768px) {
+
+    /* For mobile phones: 
+    [class*="dis"] {
+        grid-template-columns: 1fr 1fr;
+    }
+} */
+
+
+.container{
+    padding-top: 20px;
+    padding-bottom: 20px;
+}
+.cont1 {
+    max-height: 200px;
+
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+    grid-template-rows: 1fr;
 
 }
 
 @media only screen and (max-width: 768px) {
-  /* For mobile phones: */
-  [class*="cont1"] {
-    grid-template-columns: 1fr 1fr;
-  }
+    /* For mobile phones: */
+
+    [class*="cont1"] {
+        grid-template-columns: 1fr 1fr;
+        padding-bottom: 30px;
+    }
+    a > img{
+        margin-bottom: 10px;
+    }
 }
 </style>
