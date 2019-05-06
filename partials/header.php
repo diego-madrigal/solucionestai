f1ab52267b2ec9f5c4081fb53f90dede9256e4a1
Arturo Andrade <arturoandradm@gmail.com>
2019-05-06 10:24:00 -0500

    SE AGREGÓ EL MENÚ A UN ARCHIVO INDEPENDIENTE

---
+++ partials/header.php
@@ -0,0 +1,22 @@
+
+<nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand">Navbar</a>
+    <form class="form-inline">
+        <ul class="navbar-nav mr-auto flex-md-row">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Conocenos</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link" href="#">
+                    Contactanos
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+        </ul>
+    </form>
+</nav>
