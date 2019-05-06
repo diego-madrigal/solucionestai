225ca241b6428b842e3f2c62e2b747f96795631f
Arturo Andrade <arturoandradm@gmail.com>
2019-05-06 10:37:31 -0500

    SE CAMBIÓ LA BARRA DE NAVEGACION

--- index.php
+++ index.php
@@ -6,13 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Inicio | Solucionestai</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.1/css/all.css">
 </head>
 <body>
-    
+    <?php include_once("partials/header.php"); ?>
 
     <!-- SCRIPTS -->
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
+    
 </body>
 </html>
--- partials/header.php
+++ partials/header.php
@@ -1,22 +1,26 @@
+<style>
 
-<nav class="navbar navbar-dark bg-dark">
-    <a class="navbar-brand">Navbar</a>
-    <form class="form-inline">
-        <ul class="navbar-nav mr-auto flex-md-row">
+</style>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Conocenos</a>
+                <a class="nav-link" href="#">Features</a>
             </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link" href="#">
-                    Contactanos
-                </a>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
             </li>
         </ul>
-    </form>
+    </div>
 </nav>
