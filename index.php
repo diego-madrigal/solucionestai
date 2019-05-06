f4d2528b873dc3d6a811b4bf6829b0df9b7928c9
Arturo Andrade <arturoandradm@gmail.com>
2019-05-06 10:29:56 -0500

    SE AGREGARON LOS ARCHIVOS DE BOOTSTRAP

--- index.php
+++ index.php
@@ -5,8 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Inicio | Solucionestai</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
 </head>
 <body>
     
+
+    <!-- SCRIPTS -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
